90d1884b5b8d34b586e9dc0dd368f2069eb90317
ljh1114 <wnsgud1114@gmail.com>
2026-05-05 14:27:23 +0900

    feat: [이준형] 프론트 API 명세 맞춤 신규 엔드포인트 추가 [MJE-BE-38]

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from app.common.exception_handler import register_exception_handlers
 from app.domains.courses.controller.api.courses_router import router as courses_router
 from app.domains.home.controller.api.home_router import router as home_router
 from app.domains.recommendation.controller.api.recommendation_router import router as recommendation_router
+from app.domains.recommendation.controller.api.recommendations_detail_router import router as recommendations_detail_router
 from app.infrastructure.api.email.email_router import router as email_router
 from app.infrastructure.api.export_logs.export_log_router import router as export_log_router
 from app.infrastructure.cache.redis_client import close_redis
@@ -46,6 +47,7 @@ register_exception_handlers(app)
 
 app.include_router(home_router)
 app.include_router(recommendation_router)
+app.include_router(recommendations_detail_router)
 app.include_router(courses_router)
 app.include_router(export_log_router)
 app.include_router(email_router)
